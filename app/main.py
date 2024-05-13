73a99a05ed1de8498d0cabf1a4046f53138a0cf5
Pavel Grigorev <grigpasha05@gmail.com>
2024-05-13 13:00:20 +0300

    Changed the data transmission format

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ from fastapi import FastAPI, Depends
 from pydantic import BaseModel
 from app.model.model import get_predict
 
+
 app = FastAPI()
 
 
@@ -25,21 +26,11 @@ def home():
     return 'Ok'
 
 
-@app.post('/predict')
-def predicted(features: Annotated[In, Depends()]):
-    result = get_predict([
-        features.step,
-        features.amount,
-        features.oldBalanceOrig,
-        features.newBalanceOrig,
-        features.oldBalanceDes,
-        features.newBalanceDes,
-        features.CASH_OUT,
-        features.DEBIT,
-        features.PAYMENT,
-        features.TRANSFER])
-    return {'result': result}
-
+@app.post("/predict/")
+def predict(data: dict):
+    features = data['features']
+    prediction = get_predict(features)
+    return {"class": prediction}
 
 if __name__ == '__main__':
     uvicorn.run(app, host='127.0.0.1', port=8000)
--- dockerfile
+++ dockerfile
@@ -6,4 +6,6 @@ RUN pip install --no-cache-dir --upgrade -r /app/requirements.txt
 
 COPY ./app /app/app
 
-CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "80"]
+EXPOSE 8000
+
+CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "8000"]
